37b75eb35afccf2f221817d7372c5f355cdc5390
Hailey KIM <haileykim@MacBook-Pro.local>
2020-09-17 15:49:25 +0900

    First

---
+++ thermo.js
@@ -0,0 +1,18 @@
+
+
+
+function change(fahrenheit){
+
+    if(true){
+
+        var clecius = (fahrenheit-32)*5/9;
+        console.log(clecius); 
+
+    }
+
+    
+}
+
+//밑에서 함수를 호출하기전에 코딩이 완성되어야함.
+change(50);
+change(97.7);
