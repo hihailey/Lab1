a2d7938f34be12f7f721b5fbc651276e09d9feff
Hailey KIM <haileykim@MacBook-Pro.local>
2020-09-20 11:57:39 +0900

    First

--- thermo.js
+++ thermo.js
@@ -1,18 +1,11 @@
 
-
-
 function change(fahrenheit){
 
-    if(true){
-
-        var clecius = (fahrenheit-32)*5/9;
-        console.log(clecius); 
-
-    }
-
-    
+    var clecius = (fahrenheit-32)*5/9;
+    console.log(clecius); 
+  
 }
 
-//밑에서 함수를 호출하기전에 코딩이 완성되어야함.
+//함수 안의 내용이 끝나야 호출해도 뭐가 나옴
 change(50);
 change(97.7);
